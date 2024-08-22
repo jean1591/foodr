84dd2ed4c8530ea358e7a45507627e30b6c220b0
Jean Robertou <j-rob@live.fr>
2024-08-22 19:36:50 +0200

    feat: onboard user

--- src/app/(private)/onboarding/page.tsx
+++ src/app/(private)/onboarding/page.tsx
@@ -5,15 +5,28 @@ import {
   buttonHoverTransition,
   textGradient,
 } from '@/utils/design/constants'
+import {
+  setLoadingWeeklyMeals,
+  setWeeklyMeals,
+} from '@/app/lib/store/features/meals/slice'
+import { useDispatch, useSelector } from 'react-redux'
 
+import { PiArrowClockwise } from 'react-icons/pi'
+import { RootState } from '@/app/lib/store/store'
 import { Step } from './components/Step'
+import { WeeklyMeals } from '@/utils/interfaces/meals'
 import { classNames } from '@/utils/classNames'
 import useOptionsHook from '../planner/components/hook/useOptionsHook'
+import { useRouter } from 'next/navigation'
 import { useState } from 'react'
 
+// TODO: export step to custom component
 export default function OnboardingPage() {
+  const router = useRouter()
+  const dispatch = useDispatch()
   const [stepIndex, setStepIndex] = useState<number>(0)
   const options = useOptionsHook()
+  const { loadingWeeklyMeals } = useSelector((state: RootState) => state.meals)
 
   const handleStepDecrement = () => {
     if (stepIndex > 0) {
@@ -25,6 +38,28 @@ export default function OnboardingPage() {
     if (stepIndex < 3) {
       setStepIndex(stepIndex + 1)
     }
+  }
+
+  // TODO: add call to update hasCompletedOnboarding
+  const onGenerate = () => {
+    dispatch(setLoadingWeeklyMeals(true))
+    ;(async function getWeeklyMeals() {
+      const weeklyMealsResponse = await fetch('/api/meals', {
+        method: 'POST',
+        body: JSON.stringify({
+          filters: { vegetarianSelected: false, breakfastSelected: false },
+        }),
+        headers: { 'Content-Type': 'application/json' },
+      })
+
+      const { weeklyMeals } = (await weeklyMealsResponse.json()) as {
+        weeklyMeals: WeeklyMeals
+      }
+
+      dispatch(setWeeklyMeals(weeklyMeals))
+      dispatch(setLoadingWeeklyMeals(false))
+      router.push('/planner')
+    })()
   }
 
   return (
@@ -70,22 +105,42 @@ export default function OnboardingPage() {
           onClick={handleStepDecrement}
           className={classNames(
             buttonHoverTransition,
-            'w-32 rounded-lg border-2 border-slate-500 py-4 hover:border-slate-300'
+            'w-36 rounded-lg border-2 border-slate-500 py-4 hover:border-slate-300'
           )}
         >
           Back
         </button>
 
-        <button
-          onClick={handleStepIndexIncrement}
-          className={classNames(
-            bgGradient,
-            buttonHoverTransition,
-            'w-32 rounded-lg py-4 font-medium text-white shadow-lg hover:opacity-75 hover:shadow-none'
-          )}
-        >
-          {stepIndex === 3 ? 'Generate' : 'Continue'}
-        </button>
+        {stepIndex < 3 && (
+          <button
+            onClick={handleStepIndexIncrement}
+            className={classNames(
+              bgGradient,
+              buttonHoverTransition,
+              'w-36 rounded-lg py-4 font-medium text-white shadow-lg hover:opacity-75 hover:shadow-none'
+            )}
+          >
+            Continue
+          </button>
+        )}
+
+        {stepIndex === 3 && (
+          <button
+            onClick={onGenerate}
+            className={classNames(
+              bgGradient,
+              buttonHoverTransition,
+              'w-36 rounded-lg py-4 font-medium text-white shadow-lg hover:opacity-75 hover:shadow-none'
+            )}
+          >
+            <div className="flex items-center justify-center gap-x-2">
+              {loadingWeeklyMeals && (
+                <PiArrowClockwise className="h-6 w-6 animate-spin" />
+              )}
+              <p>Generate</p>
+            </div>
+          </button>
+        )}
       </div>
     </div>
   )
