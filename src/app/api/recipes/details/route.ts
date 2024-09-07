9af436810c2471406cfc859fe7da65e63abdec6d
Jean Robertou <j-rob@live.fr>
2024-09-07 17:53:22 +0200

    feat: save and get recipe details in DB

--- src/app/api/recipes/details/route.ts
+++ src/app/api/recipes/details/route.ts
@@ -1,21 +1,33 @@
 import { NextRequest, NextResponse } from 'next/server'
 import { Recipe, RecipeItem } from '@/utils/interfaces/recipes'
 
+import { DbRecipeWithRelations } from '../../interfaces/recipes'
 import OpenAI from 'openai'
 import { Options } from '@/app/lib/store/features/options/slice'
+import { SupabaseClient } from '@supabase/supabase-js'
 import { completionRecipe } from '@/utils/mocks/openai/recipe'
+import { createClient } from '@/utils/supabase/server'
+import { getLoggedInUser } from '../../utils/user'
 import { getOpenAiData } from '../../utils/getOpenAiData'
+import { isNil } from 'lodash'
 import { openAiResponseToJsonFormatter } from '../../utils/openAiResponseFormater'
 
 const openai = new OpenAI({ apiKey: process.env.OPENAI_API_KEY })
 
-// TODO: save in DB
-// TODO: fetch from DB
 export async function POST(request: NextRequest): Promise<NextResponse> {
   const {
     options,
     selectedRecipe,
   }: { options: Options; selectedRecipe: RecipeItem } = await request.json()
+  const supabase = createClient()
+  const { id: userId } = await getLoggedInUser(supabase)
+  const dbRecipe = await getDbRecipe(supabase, selectedRecipe.name, userId)
+
+  if (!isNil(dbRecipe)) {
+    console.log('Data source is DB')
+    const recipe = formatDbRecipeToRecipe(dbRecipe)
+    return NextResponse.json({ recipe })
+  }
 
   const prompt = generatePrompt(options, selectedRecipe)
   console.info(prompt)
@@ -28,17 +40,106 @@ export async function POST(request: NextRequest): Promise<NextResponse> {
   )
   console.info(recipe)
 
+  await insertRecipeInDb({ recipe, supabase, userId })
+
   return NextResponse.json({ recipe })
 }
 
 const generatePrompt = (options: Options, selectedRecipe: RecipeItem) => {
-  return `Generate a JSON object for a recipe with the following structure: { "label": string, "cookTime": number, "description": string, "ingredients": [ {"icon": string,"name": string,"quantity": number,"unit": string}],"instructions": [{"instruction": string,"stepNumber": number}],"name": string,"prepTime": number }
+  return `Generate a JSON object for a recipe with the following structure: { "label": string,"description": string, "ingredients": [ {"icon": string,"name": string,"quantity": number,"unit": string}],"instructions": [{"instruction": string,"stepNumber": number}],"name": string }
 
   Details:
   - Recipe name: ${selectedRecipe.name}
-  - Total preparation time: ${selectedRecipe.totalTime} minutes (split between prepTime and cookTime)
-  - Times must be in minutes
   - ${options.excludedIngredients.length > 0 ? `Exclude these ingredients: ${options.excludedIngredients.join(', ')}` : 'No ingredients to exclude.'}
   - Use grams, liters and minutes.
   `
+}
+
+const getDbRecipe = async (
+  supabase: SupabaseClient,
+  name: string,
+  userId: string
+): Promise<DbRecipeWithRelations | null> => {
+  const { data: recipes } = await supabase
+    .from('recipes')
+    .select('*, ingredients (*), instructions (*)')
+    .eq('user_id', userId)
+    .eq('name', name)
+
+  if (
+    isNil(recipes) ||
+    recipes.length === 0 ||
+    recipes[0].ingredients.length === 0 ||
+    recipes[0].instructions.length === 0
+  ) {
+    return null
+  }
+
+  return recipes[0]
+}
+
+const insertRecipeInDb = async ({
+  recipe,
+  supabase,
+  userId,
+}: {
+  recipe: Recipe
+  supabase: SupabaseClient
+  userId: string
+}) => {
+  const { data: recipes } = await supabase
+    .from('recipes')
+    .select('id')
+    .eq('name', recipe.name)
+    .eq('user_id', userId)
+
+  if (isNil(recipes) || recipes.length === 0) {
+    console.error(`No recipes found for ${recipe.name} for user ${userId}`)
+    throw new Error(`No recipes found for ${recipe.name} for user ${userId}`)
+  }
+
+  const { ingredients, instructions } = recipe
+
+  const { error: ingredientsInsertError } = await supabase
+    .from('ingredients')
+    .insert(
+      ingredients.map((ingredient) => ({
+        ...ingredient,
+        recipe_id: recipes[0].id,
+      }))
+    )
+  if (ingredientsInsertError) {
+    console.error('An error occured while inserting ingredients', {
+      error: JSON.stringify(ingredientsInsertError, null, 2),
+    })
+  }
+
+  const { error: instructionsInsertError } = await supabase
+    .from('instructions')
+    .insert(
+      instructions.map((instruction) => ({
+        instruction: instruction.instruction,
+        step_number: instruction.stepNumber,
+        recipe_id: recipes[0].id,
+      }))
+    )
+  if (instructionsInsertError) {
+    console.error('An error occured while inserting instructions', {
+      error: JSON.stringify(instructionsInsertError, null, 2),
+    })
+  }
+}
+
+const formatDbRecipeToRecipe = (dbRecipes: DbRecipeWithRelations): Recipe => {
+  return {
+    cookTime: dbRecipes.cook_time,
+    description: dbRecipes.description,
+    ingredients: dbRecipes.ingredients,
+    instructions: dbRecipes.instructions.map((instruction) => ({
+      instruction: instruction.instruction,
+      stepNumber: instruction.step_number,
+    })),
+    name: dbRecipes.name,
+    prepTime: dbRecipes.prep_time,
+  }
 }
